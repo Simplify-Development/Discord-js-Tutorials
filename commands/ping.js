07d4eb9c0b80a44e8402a7d4409287aa015d61a2
Zofux <70540447+Zofux@users.noreply.github.com>
2021-04-16 16:35:34 +0200

    Episode #4

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "ping",
     /** 
      * @param {Discord.Client} client
-     * @param {Disocrd.Message} message
+     * @param {Discord.Message} message
      * @param {String[]} args
     */
     async execute(client, message, args) {
